512beffc8c8738012da780de95b8da3c542bf6f7
ceelogre <ceelogre@outlook.com>
2023-01-11 00:37:47 +0200

    WIP: homepage re-design

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,62 +2,54 @@ import styles from '../styles/Index.module.css'
 const indexPage = () => {
   return (
     <div>
-     <header>
-        <div className="header-wrapper">
-          <figure className="logo">
-            <a href="index.html">
-              <p className="logo">Pi Tech</p>
-            </a>
-          </figure>
-          <nav className="nav-wrapper">
-            <ul>
-              <li><a href="./index.html#form">About</a></li>
-              <li><a href="./contact.html">Contact</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+     
       <main>
-      <div className={styles.card}>
-        <h2>CARD</h2>
-        <div className={styles.formCard}>
-          <form>
-            <div className="form-group">
-              <input type="text" className="form-control" id="username" placeholder="What should we call you?" />
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <header className={styles.carduno}>
+              <h1>We are Pi Tech</h1>
+              <h2>Nice to meet you.</h2>
+            </header>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardduo}>
+              <p>We love the number &pi;. Why? Because it has special (cool) properties.</p><br />
+              <ul>
+                <li>It has an infinite number of decimal places</li>
+                <li>The digits are non-repeating</li>
+                <li>The digits are non-terminating</li>
+              </ul>
+              <br />
+              <p>We like to think of Technology through the same lenses. A tool that gives people endless opportunity where the limit is only your imagination.</p>
             </div>
-            <div className="form-group">
-              <input type="email" className="form-control" name="email" placeholder="Your email address" required/>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardtreo}>
+              <p>Do you find that intriguing? Is there something you'd like to explore and see where it takes you? Drop us a line using the next form and we'll be happy to get in touch.</p>
             </div>
-            <div className="form-group">
-              <textarea name="text" id="text" className="form-control" placeholder="your message" title="Pro-tip: resize this window by dragging the tiles in the bottom-corner" required></textarea>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.formCard}>
+              <form>
+                <div className={styles.formGroup}>
+                  <input type="text" className={styles.formControl} id="username" placeholder="What should we call you?" />
+                </div>
+                <div className={styles.formGroup}>
+                  <input type="email" className={styles.formControl} name="email" placeholder="Your email address" required/>
+                </div>
+                <div className={styles.formGroup}>
+                  <textarea name="text" id="text" className={styles.textAreaControl} placeholder="your message" title="Pro-tip: resize this window by dragging the tiles in the bottom-corner" required></textarea>
+                </div>
+                
+                <div className={styles.formGroup}>
+                  <button id="form-submit" type="submit" className={styles.btnPrimary}>Send</button>
+                </div>
+              </form>
             </div>
-            
-            <div className="form-group">
-              <button id="form-submit" type="submit" className="btn-primary form-control">Send</button>
-            </div>
-          </form>
+          </div>
+          
         </div>
-      </div>
-        <section className="jumbotron">
-          <h2>Pi Tech</h2>
-          <p>Welcome to Pi Tech, </p>
-        </section>
-      
       </main>
-      <div className="modal closed" id="modal">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h2>Thanks! </h2>
-          <button className="btn"><span className="close-btn">&times;</span></button>
-          </div>
-          <div className="modal-body">
-            <p>Sit back! You  should receive an email with your translation within an hour.</p>
-          </div>
-          <div className="modal-footer">
-            <button className="thanks-btn">Okay!</button>
-          </div>
-        </div>
-      </div>
       </div>
   )
 }
