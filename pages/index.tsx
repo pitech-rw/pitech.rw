ba393cbbc46e11a4bc81f231abe67976f60722a4
ceelogre <ceelogre@outlook.com>
2023-01-25 12:11:32 +0200

    WIP: open modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
+import { useState, useContext} from 'react'
 import styles from '../styles/Index.module.css'
 import { collection, addDoc } from 'firebase/firestore'
 import storage from  '../service/firebase'
+import { Modal, ModalContext} from './modal'
 
 const IndexPage = () => {
 
   const [message, setMessage] = useState({})
-  const [showModal,setVisible]=useState(false);
   const updateMessage = (e: any) => {
     const {name} = e.target;
     const {value} =e.target;
     setMessage((values)=>({...values,[name]:value}))
   }
+
+  const openModal = useContext(ModalContext)
   const saveContactMessage = async (e: any) => {
     e.preventDefault()
 
     try {
-      console.info('Obj ', message)
-      const docReference = await addDoc(collection(storage, "contacts"), message)
-      setVisible(true);
+      await addDoc(collection(storage, "contacts"), message)
+      
+      if(openModal)
+        openModal();
+      else console.info(openModal, ' rented')
+      setTimeout(() => {
+      }, 10000);
     } catch (e) {
-      console.error('Unable to add a doc', e)
+    //TO DO: tracking
     }
   }
 
@@ -37,27 +43,25 @@ const IndexPage = () => {
           </div>
           <div className={styles.card}>
             <div className={styles.cardduo}>
-              <p>We love the number &pi;. Why? Because it has special (cool) properties.</p><br />
+              <p>We love the number Pi (&pi;). Why? Because it has special (cool) properties.</p><br />
               <ul>
                 <li>It has an infinite number of decimal places</li>
                 <li>The digits are non-repeating</li>
                 <li>The digits are non-terminating</li>
               </ul>
               <br />
-              <p>We like to think of Technology through the same lenses. A tool that gives people endless opportunity where the limit is only your imagination.</p>
+              <p>We like to look at Technology through the same lenses. A tool that gives people endless possibilities where the limit is only your imagination.</p>
             </div>
           </div>
           <div className={styles.card}>
             <div className={styles.cardtreo}>
-              <p>Do you find that intriguing? Is there something you &apos;d like to explore and see where it takes you? Drop us a line using the next form and we &apos; ll be happy to get in touch.</p>
+              <p>Do you find that intriguing? <br />Is there a software project you &apos;d like to explore and see where it takes you?<br />Does your website need maintainance?<br/>Drop us a line using the next form, we &apos; ll be happy to get in touch.</p>
             </div>
           </div>
           <div className={styles.card}>
+                
             <div className={styles.formCard}>
               <form onSubmit={saveContactMessage}>
-                <div className={styles.formGroup}>
-                  {showModal && (<p>Thank you for contacting us!</p>)}
-                </div>
                 <div className={styles.formGroup}>
                   <input type="text" onChange={updateMessage} className={styles.formControl} name="name" placeholder="What should we call you?" />
                 </div>
@@ -77,6 +81,10 @@ const IndexPage = () => {
           
         </div>
       </main>
+      <Modal openModal={openModal}>
+        <h2>Thanks for reaching out!</h2>
+        <p>We'll get back to you in less than 24 hours.</p>
+      </Modal>
     </div>
   )
 }
---
+++ pages/modal.tsx
@@ -0,0 +1,30 @@
+import React, {useState } from 'react'
+
+const ModalContext = React.createContext<(() => void)| undefined>(undefined)
+const Modal = ({ children }) => {
+    const [isvisible, setIsVisible] = useState(false)
+
+    const openModal = () => {
+        setIsVisible(true)
+    }
+
+    const closeModal = () => {
+        setIsVisible(false)
+    }
+
+    return (
+        <ModalContext.Provider value={openModal}>
+            {
+                isvisible && (
+                    <div>
+                        <div>
+                            <button onClick={closeModal}>&times;</button>
+                            {children}
+                        </div>
+                    </div>
+                )
+            }
+        </ModalContext.Provider>
+    )
+}
+export {ModalContext, Modal}
